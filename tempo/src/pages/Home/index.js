05a6286b77aa97e5961bb08ebafc0cb842e608f9
MarcosVel <marcos.veloso@sempreceub.com>
2021-03-17 23:55:07 -0300

    add Header

--- tempo/src/pages/Home/index.js
+++ tempo/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import Header from '../../components/Header';
 import Menu from '../../components/Menu';
 
 export default function Home() {
   return (
-    <SafeAreaView style={ styles.container }> 
+    <SafeAreaView style={ styles.container }>
+      <Header />
       <Menu />
     </SafeAreaView>
   )
